972294ce1160844b55beacd97f1d94cfbc20bf2f
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2018-02-22 14:24:34 +0100

    Fixed: \r\n / \n mixup when running on appveyor.

--- CSLib/test/CppParsing/CppParserTest.cs
+++ CSLib/test/CppParsing/CppParserTest.cs
@@ -11,19 +11,9 @@ namespace DevPal.CSLib.Tests.CppParsing
 		public void TestRemoveIncludes()
 		{
 			CppParser parser = new CppParser();
-			string contents = @"// #include ""hello.h""
-#include ""hello.h""
-
-class hello {
-};
-";
+			string contents = @"// #include ""hello.h""\n#include ""hello.h""\n\nclass hello {\n};\n";
 			parser.RemoveIncludes(ref contents);
-			string expected_contents = @"// #include ""hello.h""
-
-
-class hello {
-};
-";
+			string expected_contents = @"// #include ""hello.h""\n\n\nclass hello {\n};\n";
 			Assert.AreEqual(expected_contents, contents);
 		}
 
